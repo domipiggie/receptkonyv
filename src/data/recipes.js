e9c9abeeb03a9f4cd2ca7fe11027ca5a94e9724d
domipiggie <domipiggie@gmail.com>
2024-12-09 13:30:40 +0100

    recept data

---
+++ src/data/recipes.js
@@ -0,0 +1,34 @@
+export default recipes = [
+    {
+        id:0,
+        name:"Palacsinta",
+        prepTime:30,
+        difficulty:0,
+        description:"valami palacsinta Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        img:"palacsinta.jpg"
+    },
+    {
+        id:1,
+        name:"Paprikás krumpli",
+        prepTime:45,
+        difficulty:0,
+        description:"finom paprikás krumpli",
+        img:"paprikaskrumpli.jpg"
+    },
+    {
+        id:2,
+        name:"Túrósgombóc",
+        prepTime:60,
+        difficulty:1,
+        description:"Elfogytak az ötleteim amit ide írhatok...",
+        img:"turogomboc.jpg"
+    },
+    {
+        id:3,
+        name:"Gulyásleves",
+        prepTime:120,
+        difficulty:2,
+        description:"leveses dolog",
+        img:"gulyasleves.jpg"
+    }
+]
