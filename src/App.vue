4e79c3e92e2ed14c800e5b506ba997eb3a4fc5be
Berc57 <keindl.bercel.2020i@bankitatabanya.hu>
2024-12-09 13:46:33 +0100

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <script setup>
+import Navbar from './components/Navbar.vue';
+
 </script>
 
 <template>
+    <header>
+
+    </header>
+    <main>
+        <section>
+            <Navbar />
+        </section>
+    </main>
+    <footer>
+
+    </footer>
 </template>
 
 <style scoped>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,32 @@
 </script>
 
 <template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand">Receptkönyv</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Receptek</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Új receptek</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Kedvencek</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
 </template>
 
 <style scoped>
+    nav a{
+        background-color: rgb(250, 250, 250);
+        margin-left: 20%;
+        width: 100px;
+    }
+    
 </style>
