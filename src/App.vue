10ac3d754c0b5240c5e3f754a1e3396142421598
domipiggie <domipiggie@gmail.com>
2024-12-09 13:42:17 +0100

    lista feldolgozása, class&data javítás

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,16 @@
 <script setup>
+import {recipes} from './data/recipes';
+import Recipe from './classes/Recipe';
+
+import { ref } from 'vue'
+
+var recipeList = ref([]);
+
+recipes.forEach((recipe)=>{
+    recipeList.value.push(new Recipe(recipe));
+})
+
+console.log(recipeList.value);
 </script>
 
 <template>
--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,15 +1,17 @@
 export default class Recipe {
     id; name; prepTime; difficulty; description; img;
 
-    constructor(recipeObj) {
-        this.id = recipeObj.id;
-        this.name = recipeObj.name;
-        this.prepTime = recipeObj.prepTime;
-        this.difficulty = recipeObj.difficulty;
-        this.description = recipeObj.description;
-        this.img = recipeObj.img;
-    }
     constructor(id, name, prepTime, difficulty, description, img) {
+        if (typeof id === 'object') {
+            this.id = id.id;
+            this.name = id.name;
+            this.prepTime = id.prepTime;
+            this.difficulty = id.difficulty;
+            this.description = id.description;
+            this.img = id.img;
+            return
+        }
+
         this.id = id;
         this.name = name;
         this.prepTime = prepTime;
