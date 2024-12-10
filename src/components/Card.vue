a01666612a287d036febc185d6a0cec0decdea10
Berc57 <keindl.bercel.2020i@bankitatabanya.hu>
2024-12-10 08:57:26 +0100

    card elements got position

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,21 +1,22 @@
 <script setup>
-import { defineProps,defineEmits } from 'vue';
+import { defineProps, defineEmits } from 'vue';
 import Recipe from '../classes/Recipe';
 
 const props = defineProps({
-    recept:Recipe
+    recept: Recipe
 });
 const emits = defineEmits(['showDetails']);
 
 const difficultyList = ['Könnyű', 'Közepes', 'Nehéz'];
 
 const showDetails = (id) => {
-    emits('showDetails',id);
+    emits('showDetails', id);
 }
 
 const getImgUrl = (img) => {
-    return new URL(img, import.meta.url.replace('components','assets')).href;
+    return new URL(img, import.meta.url.replace('components', 'assets')).href;
 }
+
 </script>
 
 <template>
@@ -25,36 +26,47 @@ const getImgUrl = (img) => {
             <div class="card-body">
                 <h5 class="card-title">{{ recept.name }}</h5>
                 <p class="card-text">Elkészítési idő: {{ recept.prepTime }} perc</p>
-                <p class="card-text d1" v-if="recept.difficulty == 0">{{ difficultyList[0] }}</p>
-                <p class="card-text d2" v-if="recept.difficulty == 1">{{ difficultyList[1] }}</p>
-                <p class="card-text d3" v-if="recept.difficulty == 2">{{ difficultyList[2] }}</p>
+                <div>
+                    <p class="card-text d1" v-if="recept.difficulty == 0">{{ difficultyList[0] }}</p>
+                    <p class="card-text d2" v-if="recept.difficulty == 1">{{ difficultyList[1] }}</p>
+                    <p class="card-text d3" v-if="recept.difficulty == 2">{{ difficultyList[2] }}</p>
+                </div>
                 <a href="#" class="btn btn-primary" @click.prevent="showDetails(recept.id)">Részletek</a>
-
             </div>
         </div>
     </div>
 </template>
 
 <style scoped>
-    .cards{
-        display: flex;
-    }
-    .d1{
-        background-color: #00ff00;
-        display: inline;
-        border-radius: 7px;
-        padding: 2px 4px;
-    }
-    .d2{
-        background-color: #ffff00;
-        display: inline;
-        border-radius: 7px;
-        padding: 2px 4px;
-    }
-    .d3{
-        background-color: #ff0000;
-        display: inline;
-        border-radius: 7px;
-        padding: 2px 4px;
-    }
+.cards {
+    display: flex;
+}
+
+.card-body a {
+    display: inline-block;
+}
+
+.d1 {
+    background-color: #00ff00;
+    border-radius: 7px;
+    padding: 2px 4px;
+    display: inline;
+}
+
+.d2 {
+    background-color: #ffff00;
+    display: inline;
+    border-radius: 7px;
+    padding: 2px 4px;
+}
+
+.d3 {
+    background-color: #ff0000;
+    display: inline;
+    border-radius: 7px;
+    padding: 2px 4px;
+}
+.btn{
+    margin-top: 20px;
+}
 </style>
