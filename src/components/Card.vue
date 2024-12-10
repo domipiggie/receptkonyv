94b8addba8fce8d3a0ad56ea3a9aeceb0d03609c
domipiggie <domipiggie@gmail.com>
2024-12-10 08:15:26 +0100

    props, emits

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,4 +1,11 @@
 <script setup>
+import { defineProps,defineEmits } from 'vue';
+import Recipe from '../classes/Recipe';
+
+const props = defineProps({
+    recept:Recipe
+});
+const emits = defineEmits(['showDetails']);
 </script>
 
 <template>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,4 +1,8 @@
 <script setup>
+import { defineProps,defineEmits } from 'vue';
+import Recipe from '../classes/Recipe';
+
+const emits = defineEmits(['search']);
 </script>
 
 <template>
