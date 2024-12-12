64faed68c0b0ae399b738680005c539e7860e87d
domipiggie <domipiggie@gmail.com>
2024-12-12 08:59:47 +0100

    Részletek modal kiszervezve komponensbe

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,6 +2,7 @@
 import { defineProps, defineEmits } from 'vue';
 import Recipe from '../classes/Recipe';
 import { recipes } from '../data/recipes';
+import Details from './Details.vue';
 
 const props = defineProps({
     recept: Recipe
@@ -38,24 +39,7 @@ const getImgUrl = (img) => {
                     <a class="btn btn-primary" data-bs-toggle="modal" :data-bs-target="'#' + recept.id">Részletek</a>
                 </div>
 
-                <div class="modal fade" :id="recept.id" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-                    aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="staticBackdropLabel">{{ recept.name }}</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                {{ recept.description }}
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Bezárás</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Details :recept="recept"></Details>
             </div>
         </div>
     </div>
